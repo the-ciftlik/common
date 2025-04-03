906904d912a6c3a9f5a38dbe7e73abf76476a948
Mehmet Eren Balasar <balasar@ratioenergy.co>
2025-04-03 16:08:04 +0300

    Create constants.py

---
+++ common/constants.py
@@ -0,0 +1,25 @@
+MODEL_TIME_INTERVAL_SECONDS = 60 * 60 * 24 * 7  # 1 week
+
+DATE_COL = "Date"
+DATE_FORMAT = "%Y-%m-%d"
+
+SEED_PRICE_COL = "Seed Price"
+
+PLANTING_LABOR_PRICE_COL = "Planting Labor Price"
+MAINTENANCE_LABOR_PRICE_COL = "Maintenance Labor Price"
+HARVESTING_LABOR_PRICE_COL = "Harvesting Labor Price"
+
+MACHINERY_PRICE_COL = "Machinery Price"
+
+FERTILIZER_PRICE_COL = "Fertilizer Price"
+
+PESTICIDE_PRICE_COL = "Pesticide Price"
+
+TRANSPORTATION_PRICE_COL = "Transportation Price"
+
+IRRIGATION_PRICE_COL = "Irrigation Price"
+
+MARKET_PRICE_COL = "Market Price"
+
+# !! WEATHER FILE IS NOT IMPLEMENTED YET
+
